64a24fbb6e15973681bd67536e5d73449f77bd54
Alex Kersten <alex@kersten.email>
2018-08-08 00:10:13 -0500

    add configuration reading

--- powerline.py
+++ powerline.py
@@ -1,25 +1,92 @@
-from flask import Flask
+import os
 import logging
 import getpass
 
+from flask import Flask
 from systemd.journal import JournaldLogHandler
 
 
+# #########
+# Constants
+# #########
+
+HOMEDIR = "/home"
+STEP_FILE_NAME = ".cuteworks-powerline-steps"
+
+
+# #######
+# Globals
+# #######
+
+request_count = 0
+endpoint_map = {}
+
+# ################
+# Initialize Flask
+# ################
+
 app = Flask(__name__)
-logger = logging.getLogger(__name__)
+
+@app.before_request
+def before_request():
+    global request_count
+    request_count = request_count + 1
+
+
+# ##############
+# Set up logging
+# ##############
+
+logger = logging.getLogger("cuteworks-powerline")
 
 journald_handler = JournaldLogHandler()
-
 journald_handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 
 logger.addHandler(journald_handler)
 logger.setLevel(logging.DEBUG)
 
-@app.route('/')
-def hello_world():
-    logger.info("Test logging")
 
-    return 'Hello World! I am ' + getpass.getuser()
+# ###############
+# Read step files
+# ###############
+
+logger.debug("traversing home directories and parsing step files")
+
+homedirs = [f.path in os.scandir(HOMEDIR) if (f.is_dir() and f.name != "lost+found")]
+
+for p in homedirs:
+    if not os.path.exists(p + "/" + STEP_FILE_NAME):
+        logger.debug("  no step file in " + p)
+        continue
+    logger.debug("  found step file in " + p)
+
+    with open(p + "/" + STEP_FILE_NAME) as f:
+        for line in f:
+            split = line.split(":")
+            if len(split) != 2:
+                logger.debug("    invalid format, skipping this line: " + line)
+                continue
+            endpoint = split[0]
+            script = split[1]
+            if endpoint in endpoint_map:
+                logger.debug("    skipping endpoint /" + endpoint + ", already mapped to " + endpoint_map[endpoint])
+                continue
+
+            if not os.path.exists(script):
+                logger.debug("    script does not exist or is unreadable, skipping this line: " + line)
+                continue
+
+            logger.debug("    mapping endpoint: /" + endpoint + " -> " + script)
+            endpoint_map[endpoint] = script
+
+logger.debug("ready")
+
+@app.route('/')
+def splash():
+    logger.debug("Served splash endpoint.")
+
+    return "Hello world, powerline running as " + getpass.getuser() + " and has served " + str(request_count) +\
+           " requests this session"
 
 
 if __name__ == '__main__':
